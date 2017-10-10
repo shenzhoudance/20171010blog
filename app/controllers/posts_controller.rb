f5174a3a88ba3d58515f236907c8db22e9a45089
xiaowei <shenzhoudance@gmail.com>
2017-10-10 15:24:11 +0800

    Posts Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,46 +1,23 @@
 class PostsController < ApplicationController
 
-
   def index
-  @posts = Post.all
-end
-
-def new
-  @post = Post.new
-end
-
-def create
-  @post = Post.new(post_params)
-  if @post.save
-    redirect_to @post
-  else
-    render 'new'
+    @posts = Post.all
   end
-end
+
+  def new
+  end
+
 
   def show
     @post = Post.find(params[:id])
   end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
 
-  def update
-    @post = Post.find(params[:id])
+  def create
+    @post = Post.new(post_params)
+    @post.save
 
-    if @post.update(params[:post].permit(:title, :body))
-      redirect_to @post
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
-
-    redirect_to posts_path
+    redirect_to @post
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   resources :posts
   root "posts#index"
 
-  resources :posts do
-    resources :comments
-  end
+
 
 end
